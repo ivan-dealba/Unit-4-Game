4c8f710516b2da1e43b64b002936eedbe9b4002a
Ivan De Alba <ivan_dealba@outlook.com>
2019-07-08 13:50:12 -0500

    Number randomly generated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,5 +2,5 @@ $(document).ready(function(){
     var randomNum = Math.floor((Math.random() * 51) + 25);
     console.log(randomNum);
 
-    // $('#random-number').
+    $('#random-number').text(randomNum);
 })
